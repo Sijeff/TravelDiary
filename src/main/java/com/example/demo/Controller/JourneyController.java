e122a4a7d96159660ae4520c68ee5e7273a1a814
Simon Pellijeff <simonpellijeff@gmail.com>
2018-04-10 16:15:48 +0200

    Enable navigation to /journeys

---
+++ src/main/java/com/example/demo/Controller/JourneyController.java
@@ -0,0 +1,14 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class JourneyController {
+
+    @GetMapping("/journeys/")
+    public ModelAndView listJourneys() {
+        return null;
+    }
+}
